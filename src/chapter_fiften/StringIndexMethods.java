11507419d4d5b143cc6da11c918e6fa0b3409fd1
forverchild <dabreezy8@gmail.com>
2021-11-23 07:25:00 +0100

    today

---
+++ src/chapter_fiften/StringIndexMethods.java
@@ -0,0 +1,9 @@
+package chapter_fiften;
+
+public class StringIndexMethods {
+    public static void main(String[] args) {
+        String letters = "abcdreuiosplskjdhddgdhdjdhd";
+
+        System.out.printf("'c' is located at index %d", letters.indexOf('c'));
+    }
+}
